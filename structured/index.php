d375d239e2d825c93344200b0f51b48843e1611e
mr-zero-03 <danielsantiagoperez03@gmail.com>
2021-10-27 12:03:04 -0500

    Starting with the CRUD for structures (This is going to be make based in the multi-array CRUD but using hashes [associative arrays])

---
+++ structured/index.php
@@ -0,0 +1,34 @@
+<?php
+  $title = "Multiple Structs";
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    <meta charset="UTF-8"/>
+    <title>CRUD <?= $title; ?></title>
+  </head>
+
+  <body>
+
+    <h1>CRUD <?= $title; ?></h1>
+
+    <hr/>
+
+    <p>
+      CRUD made using <?= $title; ?>.
+    </p>
+
+    <ol>
+	    <li> <a href="clients/">Clients</a> </li>
+	    <li> <a href="students/">Students</a> </li>
+	    <li> <a href="teachers/">Teachers</a> </li>
+    </ol>
+
+    <a href="../">Go back to the CRUDs selection</a>
+
+  </body>
+
+</html>

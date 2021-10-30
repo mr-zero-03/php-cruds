2d36eea8a7eb1690a29929da592b07d0950f2b50
mr-zero-03 <danielsantiagoperez03@gmail.com>
2021-10-30 14:56:23 -0500

    The struct now is contained in a YAML file

--- structured/clients/libs/loader.php
+++ structured/clients/libs/loader.php
@@ -1,5 +1,5 @@
 <?php
-  include_once( $referencePath . 'libs/struct-name.php' );
+  include_once( $referencePath . 'struct.php' );
   include_once( $referencePath . 'libs/users.php' );
   include_once( $referencePath . 'libs/helpers.php' );
   include_once( $referencePath . 'libs/form.php' );
--- structured/clients/struct.php
+++ structured/clients/struct.php
@@ -1,7 +1,12 @@
 <?php
 
-  $singular = 'Client';
-  $plural = 'Clients';
+  $file_name = 'struct.yaml';
+  $yaml = yaml_parse_file( $referencePath . $file_name );
+  $yamlStruct = $yaml[ 'struct' ];
+
+  //Struct Name------
+  $singular = $yamlStruct[ 'singular_name' ];
+  $plural = $yamlStruct[ 'plural_name' ];
 
   $structName = array (
     'properSingular' => $singular, //Proper Case
@@ -15,3 +20,7 @@
 
     'forcode' => strtolower ( $singular ) //Used on the code using lowercase
   );
+
+
+  //Struct Fields------
+  $struct = $yamlStruct[ 'fields' ];
